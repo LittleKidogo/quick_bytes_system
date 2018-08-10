6c92b630abe45c45dcd7265e25a7717a90375727
Wanjiku Mac <edwardscaroline32@gmail.com>
2018-08-11 01:18:30 +0300

    added the tag tests

---
+++ priv/repo/migrations/20180810220051_create_tag.exs
@@ -0,0 +1,14 @@
+defmodule QbBackend.Repo.Migrations.CreateTag do
+  use Ecto.Migration
+
+  def change do
+    create table(:tags, primary_key: false) do
+      add :id, :binary_id, primary_key: true
+      add :name, :string
+
+      timestamps()
+    end
+
+  end
+end
+
---
+++ test/lib/qb_backend/posts/tag_tests.exs
@@ -0,0 +1,32 @@
+defmodule QbBackend.Posts.TagTest do
+  @moduledoc"""
+    This module holds tests for the changeset functions under tag
+  """
+  use QbBackend.DataCase
+
+  alias QbBackend.{
+    Posts.Tag
+    }
+  @valid_tag %{name: "@cshikoh"}
+  @invalid_tag %{}
+
+  describe "Tag Test" do
+    test "Valid if the name parameter is available" do
+      changeset = Tag.changeset(%Tag{},@valid_tag)
+      assert changeset.valid?
+    end
+    test "Invalid if the name parameter is not right" do
+      changeset = Tag.changeset(%Tag{},@invalid_tag)
+      refute changeset.valid?
+
+    end
+  end
+  test "should associate a tag to a manual" do
+    tag = insert(:tag)
+    manual = insert(:manual, @valid_tag)
+    tag = tag |> Repo.preload(:manual)
+    changeset = tag.add_to_manual(tag,manual)
+    assert changeset.valid?
+    assert changeset.changes.tag
+  end
+end
