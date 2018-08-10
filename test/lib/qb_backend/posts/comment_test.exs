31c9023082b94840d5f42b86a2df070627d26870
br4inii <moshericoe@gmail.com>
2018-08-10 12:49:45 +0300

    added create_changeset function test

--- test/lib/qb_backend/posts/comment_test.exs
+++ test/lib/qb_backend/posts/comment_test.exs
@@ -6,13 +6,16 @@ defmodule QbBackend.Posts.CommentTest do
   use QbBackend.DataCase
 
   alias QbBackend.{
-    Posts.Comment
+    Posts.Comment,
+    Accounts.Profile
   }
 
   @valid_params %{body: "To ensure that you pass all the tests,
   the best but dumbest thing to do is skipping and ignoring them tests all together.
   Then you'll be having nightmares of bugs you been getting."}
   @invalid_params %{}
+  @updated_params %{body: "updated the comment"}
+
 
   describe "comments changesets" do
     test "valid comment" do
@@ -24,6 +27,10 @@ defmodule QbBackend.Posts.CommentTest do
       changeset = Comment.changeset(%Comment{}, @invalid_params)
       refute changeset.valid?
     end
-  end
 
+    test "comments update changeset" do
+      changeset = Comment.create_changeset(%Profile{},@updated_params)
+      assert changeset.valid?
+    end
+  end
 end
