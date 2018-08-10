a24846b13b67f730302484a6bf7b3cd191f20b4e
Zacck Osiemo <coderv63@gmail.com>
2018-08-10 05:35:53 +0200

    add test helpers to authenticate connections when testing the api

---
+++ test/support/api_case.ex
@@ -0,0 +1,59 @@
+defmodule QbBackend.APICase do
+  @moduledoc """
+  This module defines the setup for tests requiring
+  access to the application's GraphQL API layer.
+
+  You may define functions here to be used as helpers in
+  your tests.
+
+  Finally, if the test case interacts with the database,
+  it cannot be async. For this reason, every test runs
+  inside a transaction which is reset at the beginning
+  of the test unless the test case is marked as async.
+  """
+
+  use ExUnit.CaseTemplate
+  use Phoenix.ConnTest
+  import QbBackend.Factory
+
+  using do
+    quote do
+      alias QbBackend.Repo
+
+      import Ecto
+      import Ecto.Changeset
+      import Ecto.Query
+      import QbBackend.DataCase
+      import QbBackend.Factory
+    end
+  end
+
+
+  setup tags do
+    :ok = Ecto.Adapters.SQL.Sandbox.checkout(QbBackend.Repo)
+
+    unless tags[:async] do
+      Ecto.Adapters.SQL.Sandbox.mode(QbBackend.Repo, {:shared, self()})
+    end
+
+    # get the current profile using a tag
+    # it the test has the authenticated tag the conn is given a profile to auth
+    # with this should be extended to allow types of profiles
+    {conn, current_profile} = cond do
+    tags[:authenticated] ->
+      build_conn()
+      |> add_auth_header(tags[:authenticated])
+    true ->
+      conn = build_conn()
+      {conn, nil}
+    end
+
+    {:ok, conn: conn, current_profile: current_profile}
+  end
+
+  defp add_auth_header(conn, true) do
+    profile = insert(:profile)
+
+    conn |> QbBackend.AuthenticationTestHelpers.authenticate(profile)
+  end
+end
---
+++ test/support/authentication_test_helpers.ex
@@ -0,0 +1,30 @@
+
+defmodule QbBackend.AuthenticationTestHelpers do
+  use Phoenix.ConnTest
+  import QbBackend.Factory
+
+  @doc """
+  when given a connection to authenticate create a user call auth with the user
+  and add a token in the request header just a client would do when calling the
+  API
+  """
+  def authenticate(conn) do
+    profile = insert(:profile)
+    conn
+    |> authenticate(profile)
+  end
+
+
+  @doc """
+  when given a connection to authenticate with a user create a jwt token for the
+  user and attacj it as a request header to the connection
+  """
+  def authenticate(conn, profile) do
+    # get the token for the user
+    {:ok, token, _} = profile |> QbBackend.Guardian.encode_and_sign()
+
+    # add the users token to the request header
+    conn
+    |> put_req_header("authorization", "Bearer #{token}")
+  end
+end
