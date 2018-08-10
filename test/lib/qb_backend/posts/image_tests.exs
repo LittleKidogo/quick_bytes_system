e765c85cde20c12c86ba2f4c2b7ef221c1658b3d
br4inii <moshericoe@gmail.com>
2018-08-10 11:53:05 +0300

    added test for the image module

---
+++ test/lib/qb_backend/posts/image_tests.exs
@@ -0,0 +1,28 @@
+defmodule QbBackend.Posts.ImageTest do
+  @moduledoc """
+  this module comprises for tests for the images model
+  """
+
+  use QbBackend.DataCase
+
+  alias QbBackend.{
+    Posts.Image
+  }
+
+  @valid_params %{image_link: "https://commons.wikimedia.org/wiki/File:20091007_Graffiti_Shanghai_7373.jpg", name: "2D"}
+  @no_image_params %{name: "no image"}
+
+
+  describe "image changesets" do
+    test "valid image" do
+      changeset = Image.changeset(%Image{},@valid_params)
+      assert changeset.valid?
+    end
+
+    test "invalid/blank image" do
+      changeset = Image.changeset(%Image{}, @no_image_params)
+      refute changeset.valid?
+    end
+  end
+
+end
