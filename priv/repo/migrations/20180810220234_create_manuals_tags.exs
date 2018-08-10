74b40ff30505a43d698e7c867d514bf40c17eab8
Caroline W Macharia <32665021+WanjikuMac@users.noreply.github.com>
2018-08-11 01:46:22 +0300

    Update 20180810220234_create_manuals_tags.exs
    specified the timestamps

--- priv/repo/migrations/20180810220234_create_manuals_tags.exs
+++ priv/repo/migrations/20180810220234_create_manuals_tags.exs
@@ -4,7 +4,7 @@ defmodule QbBackend.Repo.Migrations.CreateManualTags do
     create table(:manual_tags) do
       add :tag_id, references(:manuals, on_delete: :nothing, type: :binary_id)
       add :manual_id, references(:tags, on_delete: :nothing, type: :binary_id)
-      timestamps()
+      timestamps(inserted_at: :added_on, updated_at: :edited_on)
     end
     create unique_index(:manual_tags, [:tag_id, :manual_id])
   end
