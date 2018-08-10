95f5ae12ae76a97e5e8b8fc14687669c5eee012a
br4inii <moshericoe@gmail.com>
2018-08-10 20:32:32 +0300

    create image migration

--- priv/repo/migrations/20180809164108_create_images.exs
+++ priv/repo/migrations/20180809164108_create_images.exs
@@ -6,10 +6,9 @@ defmodule QbBackend.Repo.Migrations.CreateImages do
       add :id, :binary_id, primary_key: true
       add :name, :string
       add :image_link, :string
-      #add :manual_id, references(:manuals, type: :binary_id)
       add :profile_id, references(:profiles, type: :binary_id)
 
-      timestamps()
+      timestamps(inserted_at: :added_on, updated_at: :edited_on)
     end
 
   end
