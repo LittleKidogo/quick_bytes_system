2b75d37fe9d68eedadcabfa8393dacb7a97e5db6
br4inii <moshericoe@gmail.com>
2018-08-10 16:23:33 +0300

    updated attributes for the create_changeset function

--- lib/qb_backend/posts/posts.ex
+++ lib/qb_backend/posts/posts.ex
@@ -1,13 +1,16 @@
 defmodule QbBackend.Posts do
-  @doc """
+  @moduledoc """
   This module takes the id of a manual and proceeds to find the associated Manual
   """
 
   alias QbBackend.{
       Repo,
-      Posts.Manual
+      Posts.Manual,
+      Accounts.Profile
     }
-   @doc """
+
+
+  @doc """
    This function takes the manual identification and proceeds to find the associated manual
   """
   @spec get_manual(String.t()) :: {:ok, Manual.t()} | {:error, String.t()}
@@ -15,22 +18,25 @@ defmodule QbBackend.Posts do
     with %Manual{} = manual <- Repo.get_by(Manual, id: id) do
       {:ok, manual}
     else
-      nil -> {:error, "No Manual with id #{id} on the system"}
+      nil -> {:error, "No Manual with id: #{id} on the system"}
     end
   end
+
   @doc """
     This function takes the map attributes and saves a manual record with that map of attributes
   """
-  @spec create_manual(map()) :: {:ok, Manual.t()} | {:error, Ecto.Changeset.t()}
-  def create_manual(attrs) do
-    %Manual{}
-    |> Manual.changeset(attrs)
+  @spec create_manual(Profile.t(), map()) :: {:ok, Manual.t()} | {:error, Ecto.Changeset.t()}
+  def create_manual(%Profile{} = prof, attrs) do
+    prof
+    |> Manual.create_changeset(attrs)
     |> Repo.insert()
   end
+
+
   @doc """
   This function takes a manual and a map of update attributes that it uses to
   update the manual
-"""
+  """
   @spec update_manual(Manual.t(), map()) :: {:ok, Manual.t()} | {:error, Ecto.Changeset.t()}
   def update_manual(%Manual{} = manual, attrs) do
     manual
