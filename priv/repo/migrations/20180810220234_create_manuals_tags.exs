60639ee997a5347709acc3ba6e92ab08b42134e2
Zacck Osiemo <coderv63@gmail.com>
2018-08-11 01:17:02 +0200

    rename join table

--- priv/repo/migrations/20180810220234_create_manuals_tags.exs
+++ priv/repo/migrations/20180810220234_create_manuals_tags.exs
@@ -1,11 +1,11 @@
 defmodule QbBackend.Repo.Migrations.CreateManualTags do
   use Ecto.Migration
   def change do
-    create table(:manual_tags) do
+    create table(:manuals_tags) do
       add :tag_id, references(:manuals, on_delete: :nothing, type: :binary_id)
       add :manual_id, references(:tags, on_delete: :nothing, type: :binary_id)
       timestamps(inserted_at: :added_on, updated_at: :edited_on)
     end
-    create unique_index(:manual_tags, [:tag_id, :manual_id])
+    create unique_index(:manuals_tags, [:tag_id, :manual_id])
   end
 end
