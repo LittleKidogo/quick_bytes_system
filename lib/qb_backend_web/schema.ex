c6aaf4769d0d32c652fb8aa26bf4bfaa275b6921
br4inii <moshericoe@gmail.com>
2018-08-14 20:14:18 +0300

    added create manual mutation

--- lib/qb_backend_web/schema.ex
+++ lib/qb_backend_web/schema.ex
@@ -1,11 +1,11 @@
 defmodule QbBackendWeb.Schema do
   use Absinthe.Schema
-  use Absinthe.Schema.Notation
 
   alias QbBackendWeb.Resolvers
 
   # import types
   import_types(__MODULE__.AccountTypes)
+  import_types(__MODULE__.PostTypes)
 
   query do
     @desc "The list of available users"
@@ -17,8 +17,14 @@ defmodule QbBackendWeb.Schema do
   mutation do
     @desc "user login"
     field :login, :session do
-      arg :input, non_null(:session_input)
+      arg(:input, non_null(:session_input))
       resolve(&Resolvers.Accounts.login/3)
+    end
+
+    @desc "create a manual"
+    field :add_manual, :manual do
+      arg(:input, non_null(:add_manual_input))
+      resolve(&Resolvers.Posts.add_manual/3)
     end
   end
 end
