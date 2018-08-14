fee77042cf495c851e50e123d5c9974d9222c9d5
br4inii <moshericoe@gmail.com>
2018-08-14 20:15:55 +0300

    define post types

---
+++ lib/qb_backend_web/schema/post_types.ex
@@ -0,0 +1,20 @@
+defmodule QbBackendWeb.Schema.PostTypes do
+  @moduledoc """
+  This module contains GraphQL types used with
+  the accounts context.
+  """
+  use Absinthe.Schema.Notation
+
+  @desc "manual type"
+  object :manual do
+    field(:title, :string)
+    field(:body, :string)
+  end
+
+  @desc "input to create a manual on the system"
+  input_object :add_manual_input do
+    field(:title, non_null(:string))
+    field(:body, non_null(:string))
+    field(:profile_id, non_null(:id))
+  end
+end
