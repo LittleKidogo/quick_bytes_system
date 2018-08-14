11e7b8701809f1c0530af79d7278b83fda22e94b
br4inii <moshericoe@gmail.com>
2018-08-14 20:15:09 +0300

    add posts context resolvers

---
+++ lib/qb_backend_web/resolvers/posts.ex
@@ -0,0 +1,21 @@
+defmodule QbBackendWeb.Resolvers.Posts do
+  @moduledoc """
+  A Module to hand web facing interactions for the posts context
+  """
+  alias QbBackend.{
+    Accounts,
+    Accounts.Profile,
+    Posts,
+    Posts.Manual
+  }
+
+  @spec add_manual(any(), map(), any()) :: {:ok, Manual.t()} | {:error, String.t()}
+  def add_manual(_, %{input: params}, _) do
+    with {:ok, %Profile{} = profile} <- Accounts.get_profile(params.id),
+         {:ok, %Manual{} = mnl} <- Posts.create_manual(profile, params) do
+      {:ok, mnl}
+    else
+      {:error, reason} -> {:error, reason}
+    end
+  end
+end
