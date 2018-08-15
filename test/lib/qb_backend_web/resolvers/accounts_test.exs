228ee5e606d1d032f4767724e7a1a97e9721f178
Wanjiku Mac <edwardscaroline32@gmail.com>
2018-08-15 21:01:17 +0300

    add test for account resolver

--- test/lib/qb_backend_web/resolvers/accounts_test.exs
+++ test/lib/qb_backend_web/resolvers/accounts_test.exs
@@ -1,10 +1,53 @@
 defmodule QbBackendWeb.Resolvers.AccountsTest do
   use QbBackendWeb.ApiCase
 
-  alias QbBackend.Accounts.User
+  alias QbBackend.{
+    Accounts.User,
+    Accounts.Profile
+  }
 
   @num 5
   describe "Accounts Resolver" do
+    test "logs a user on to system", %{conn: conn} do
+      {:ok, user} = User.changeset(%User{}, %{name: "zacck", hash: "12345"}) |> Repo.insert()
+      profile =  insert(:profile, user: user)
+
+
+      assert Repo.aggregate(User, :count, :id) == 1
+      assert Repo.aggregate(Profile, :count, :id) == 1
+
+      variables = %{
+        "input" => %{
+          "hash" => "12345",
+          "username" => profile.username
+        }
+      }
+
+      query = """
+      mutation($input: SessionInput!) {
+        login(input: $input){
+          token
+          profile {
+            role
+            username
+          }
+        }
+      }
+      """
+
+      res = post(conn, "api/graphiql", query: query, variables: variables)
+
+      %{
+        "data" =>  %{
+          "login" =>  result
+        }
+      } = json_response(res, 200)
+
+      assert result["token"]
+      assert result["profile"]["role"] == profile.role
+      assert result["profile"]["username"] == profile.username
+    end
+
     test "fetches users on the system", %{conn: conn} do
       insert_list(@num, :user)
       assert Repo.aggregate(User, :count, :id) == @num
@@ -24,9 +67,7 @@ defmodule QbBackendWeb.Resolvers.AccountsTest do
         "data" => %{
           "users" => people
         }
-      }
-      = json_response(res, 200)
-
+      } = json_response(res, 200)
 
       assert Enum.count(people) == @num
     end
