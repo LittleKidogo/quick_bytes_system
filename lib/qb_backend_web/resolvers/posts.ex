1b80e59443175daeb69f7b3dac9c6f870fc34be9
br4inii <moshericoe@gmail.com>
2018-08-17 23:52:22 +0300

    add comment creation resolver

--- lib/qb_backend_web/resolvers/posts.ex
+++ lib/qb_backend_web/resolvers/posts.ex
@@ -4,7 +4,10 @@ defmodule QbBackendWeb.Resolvers.Posts do
   """
   alias QbBackend.{
     Posts,
-    Posts.Manual
+    Posts.Manual,
+    Posts.Comment,
+    Accounts.Profile,
+    Accounts
   }
 
   @doc """
@@ -15,6 +18,17 @@ defmodule QbBackendWeb.Resolvers.Posts do
   def add_manual(_, %{input: params}, %{context: %{current_profile: profile}}) do
     with {:ok, %Manual{} = mnl} <- Posts.create_manual(profile, params) do
       {:ok, mnl}
+    end
+  end
+
+  @doc """
+  This function adds a comment to a manual
+  """
+  @spec add_comment(any(), map(), any()) :: {:ok, Comment.t()} | {:error, String.t()}
+  def add_comment(_, %{input: attr}, %{context: %{current_profile: profile}}) do
+    with {:ok, %Manual{} = manual} <- Posts.get_manual(attr.manual_id),
+         {:ok, %Comment{} = comment} <- Posts.add_comment(profile, manual, attr) do
+      {:ok, comment}
     end
   end
 end
