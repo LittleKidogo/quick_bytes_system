2e4410e4e68ab9814ab8405bcaf3c291a8ddcba2
br4inii <moshericoe@gmail.com>
2018-08-17 23:56:31 +0300

    add create comment associated tests

--- test/lib/qb_backend_web/resolvers/posts_test.exs
+++ test/lib/qb_backend_web/resolvers/posts_test.exs
@@ -2,6 +2,7 @@ defmodule QbBackendWeb.Resolvers.PostsTest do
   use QbBackendWeb.ApiCase
 
   @manual_attrs %{body: "some body", title: "some title"}
+  @body "this is a body"
   describe "Posts Resolver" do
     @tag authenticated: "publisher"
     test "add_manual adds a manual on the system if a user is a publisher", %{conn: conn} do
@@ -32,5 +33,64 @@ defmodule QbBackendWeb.Resolvers.PostsTest do
       assert manual["body"] == @manual_attrs.body
       assert manual["title"] == @manual_attrs.title
     end
+  end
+
+  @tag authenticated: "reader"
+  test " add a comment on a manual ", %{conn: conn} do
+    manual = insert(:manual)
+
+    variables = %{
+      "input" => %{
+        "body" => @body,
+        "manual_id" => manual.id
+      }
+    }
+
+    query = """
+    mutation ($input: AddCommentInput!) {
+            addComment(input: $input) {
+              body
+            }
+          }
+    """
+
+    res = post(conn, "api/graphiql", query: query, variables: variables)
+
+
+    %{
+      "data" => %{
+        "addComment" => comment
+      }
+    } = json_response(res, 200)
+
+    assert comment["body"] == @body
+  end
+
+  test " add a comment errors out when users is not signed in ", %{conn: conn} do
+    manual = insert(:manual)
+
+    variables = %{
+      "input" => %{
+        "body" => @body,
+        "manual_id" => manual.id
+      }
+    }
+
+    query = """
+    mutation ($input: AddCommentInput!) {
+            addComment(input: $input) {
+              body
+            }
+          }
+    """
+
+    res = post(conn, "api/graphiql", query: query, variables: variables)
+
+
+    %{
+      "errors" => [error]
+    } = json_response(res, 200)
+
+    assert error["message"] == "unauthorized"
   end
 end
