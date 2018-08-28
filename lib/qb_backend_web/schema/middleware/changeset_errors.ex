6652b55aeaee1289dca95a03fa4a0824f9c5e4ae
Zacck Osiemo <coderv63@gmail.com>
2018-08-28 20:05:09 +0200

    modify changeset errors to use options in ecto errors

--- lib/qb_backend_web/schema/middleware/changeset_errors.ex
+++ lib/qb_backend_web/schema/middleware/changeset_errors.ex
@@ -14,12 +14,21 @@ defmodule QbBackendWeb.Schema.Middleware.ChangesetErrors do
     %{res | errors: Enum.flat_map(res.errors, &transform_error/1)}
   end
 
+  @spec transform_error(Ecto.Changeset.t()) :: String.t()
   defp transform_error(%Ecto.Changeset{} = changeset) do
     changeset
-    |> Ecto.Changeset.traverse_errors(fn {err, _opts} -> err end)
+    |> Ecto.Changeset.traverse_errors(&build_error/1)
     |> Enum.map(fn {item, error} -> "#{item}: #{error}" end)
   end
 
   # handle other kinds of errors
   defp transform_error(error), do: [error]
+  
+  # build error using options
+  @spec build_error(tuple()) :: String.t()
+  defp build_error({msg, opts}) do
+    Enum.reduce(opts, msg, fn({key, value}, acc) ->
+      String.replace(acc, "%{#{key}}", to_string(value))
+    end)
+  end 
 end
