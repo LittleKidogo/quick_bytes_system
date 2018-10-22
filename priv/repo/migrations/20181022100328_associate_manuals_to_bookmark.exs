52344efb5ae7490c828c1788a731c71c3a7db907
WanjikuMac <edwardscaroline32@gmail.com>
2018-10-22 20:49:06 +0300

    This is the alter manuals table

---
+++ priv/repo/migrations/20181022100328_associate_manuals_to_bookmark.exs
@@ -0,0 +1,11 @@
+defmodule QbBackend.Repo.Migrations.AssociateManualsToBookmark do
+  use Ecto.Migration
+
+  def change do
+    alter table(:manuals) do
+      add :bookmark_id, references(:bookmarks, type: :binary_id)
+    end
+
+    create index(:manuals, [:bookmark_id])
+  end
+end
