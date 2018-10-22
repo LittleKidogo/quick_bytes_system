d85cda67ca63be2c23eac2cf6e260bb20e602420
WanjikuMac <edwardscaroline32@gmail.com>
2018-10-22 20:48:14 +0300

    removed the alter manuals table

--- priv/repo/migrations/20181018113814_create_bookmark.exs
+++ priv/repo/migrations/20181018113814_create_bookmark.exs
@@ -6,15 +6,10 @@ defmodule QbBackend.Repo.Migrations.CreateBookmark do
       add :id, :binary_id, primary_key: true
       add :manual_id, references(:manuals, type: :binary_id)
       add :profile_id, references(:profiles, type: :binary_id)
+      add :category, :string
 
       timestamps(inserted_at: :added_on, updated_at: :updated_on)
     end
-
-    alter table(:manuals) do
-      add :bookmark_id, references(:bookmarks, type: :binary_id)
-    end
-
     create index(:bookmarks, [:manual_id])
-    create index(:manuals, [:bookmark_id])
   end
 end
